91014942d2e0b43bbdc271e21b07231a81629e70
阳光阿盖尔 <wj52030000@outlook.com>
2024-11-03 19:49:34 +0800

    nacos配置管理-两种方式实现配置热更新

---
+++ user-service/src/main/java/cn/itcast/user/config/PatternProperties.java
@@ -0,0 +1,18 @@
+package cn.itcast.user.config;
+
+import lombok.Data;
+import org.bouncycastle.cms.PasswordRecipient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: hjm
+ * @Date: 2024/11/03/19:44
+ * @Description:
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "pattern")
+public class PatternProperties {
+    private String dateformat;
+}
