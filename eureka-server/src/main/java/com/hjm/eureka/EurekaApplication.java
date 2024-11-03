20b790b3e5cec4d352e70b3e3562e9aa0beab12d
阳光阿盖尔 <wj52030000@outlook.com>
2024-11-03 19:20:21 +0800

    微服务项目搭建、Eureka、nacos注册中心的学习，服务集群的搭建，Ribbon负载均衡策略、饥饿加载

---
+++ eureka-server/src/main/java/com/hjm/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hjm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: hjm
+ * @Date: 2024/11/03/16:34
+ * @Description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
